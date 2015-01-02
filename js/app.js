8884b9e03dda902f9fbdf3897d1421b0c4484101
mscosti <mscosti3@gmail.com>
2015-01-02 17:16:01 -0500

    added keyboard handling and moving of player character

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 3;
+    this.y = 3;
 }
 
 // Update the enemy's position, required method for game
@@ -18,18 +20,51 @@ Enemy.prototype.update = function(dt) {
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
 }
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
+    this.x = 0;
+    this.y = 0;
+    this.sprite = 'images/char-boy.png';
+}
+Player.prototype.update = function(dt) {
 
+}
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
+}
+
+Player.prototype.handleInput = function(direction) {
+    switch(direction){
+        case 'down':
+            this.y++;
+            break;
+        case 'up':
+            this.y--;
+            break;
+        case 'left':
+            this.x--;
+            break;
+        case 'right':
+            this.x++;
+            break;
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var numEnemies = 1;
+allEnemies = [];
+for (var i = 0; i < numEnemies; i++){
+    allEnemies.push(new Enemy());
+}
 
+player = new Player();
 
 
 // This listens for key presses and sends the keys to your
